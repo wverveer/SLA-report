b53b70ed253f69190185fb29236c5f692a270e93
Louis Lim <louis.lim@keylane.com>
2022-04-29 15:35:22 +0200

    Create converter

---
+++ src/main/java/com/keylane/JSONReader.java
@@ -0,0 +1,44 @@
+package com.keylane;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class JSONReader implements ConvertToSLARecord {
+
+    @Override
+    public List<SLARecord> convert(String inputFilePath) throws Exception {
+        List<SLARecord> SLARecordsList = new ArrayList<>();
+
+        List<JSONObject> JSONObjectList = createJSONObjectListFromInput(inputFilePath);
+        for (JSONObject object : JSONObjectList) {
+            SLARecordsList.add(createSLARecord(object));
+        }
+        return SLARecordsList;
+    }
+
+    private List<JSONObject> createJSONObjectListFromInput(String inputFilePath) throws Exception {
+        List<JSONObject> JSONObjectList = new ArrayList<>();
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader(inputFilePath)) {
+            JSONArray events = (JSONArray) ((JSONObject) jsonParser.parse(reader)).get("events");
+            for (Object object : events) {
+                JSONObjectList.add((JSONObject) object);
+            }
+        }
+        return JSONObjectList;
+    }
+
+    private SLARecord createSLARecord(JSONObject jsonObject) throws Exception {
+        Date timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(jsonObject.get("timestamp").toString());
+        String serviceName = jsonObject.get("service").toString();
+        int durationInMs = Integer.parseInt(jsonObject.get("duration_in_ms").toString());
+        return new SLARecord(timestamp, serviceName, durationInMs);
+    }
+}
