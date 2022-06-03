8201c90afb82bd32b4e1e6c991705d9c44b302cb
Vasileios Lymperakis <vasileios.lymperakis@keylane.com>
2022-06-03 15:14:41 +0200

    Add writer implementation for FileWriterImpl class

---
+++ src/main/java/com/keylane/FileWriterImpl.java
@@ -0,0 +1,19 @@
+package com.keylane;
+
+import java.io.File;
+import java.util.List;
+
+public class FileWriterImpl implements FileWriter {
+    @Override
+    public void write(List<String> output, String filename) throws Exception {
+        File myObj = new File(filename);
+        myObj.createNewFile();
+
+        java.io.FileWriter myWriter = new java.io.FileWriter(filename);
+
+        for (String s : output) {
+            myWriter.write(s);
+        }
+        myWriter.close();
+    }
+}
